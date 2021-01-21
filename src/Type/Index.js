e18aa9f623139daa0088d9454c49299dc23e7967
Vetrivel Shanmugam <vetrivel.shanmugam@sap.com>
2021-01-21 12:49:36 +0530

    Adds types

--- src/Type/Index.js
+++ src/Type/Index.js
@@ -10,13 +10,44 @@ class Type extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            types: []
         };
+    }
+
+    updateChanges(e){
+        var types = this.state.types;
+        types[Number(e.currentTarget.getAttribute("index"))] = e.currentTarget.value;
+        this.setState({
+            types: types
+        })
+    }
+
+    addInput(){
+        var types = this.state.types;
+        types.push("")
+        this.setState(
+            {types:types}
+        )
+    }
+
+    deleteIfEmpty(e){
+        var types = this.state.types;
+        if(e.currentTarget.value===""){
+            types.splice(Number(e.currentTarget.getAttribute("index")),1)
+        }
+        this.setState({
+            types: types
+        })
     }
 
     render() {
         return (
-            <div>
-                Type
+            <div className="panel">
+                <header>Type</header>
+                <div className="list">
+                    {this.state.types.map((type, index)=><textarea value={type} key={index} index={index} onChange={this.updateChanges.bind(this)} onBlur={this.deleteIfEmpty.bind(this)}/>)}
+                </div>
+                <button onClick={this.addInput.bind(this)}>Add</button>
             </div>
         );
     }
