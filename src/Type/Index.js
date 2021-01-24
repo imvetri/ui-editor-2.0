721f8ab4905d04b3d02ee2d0360ab195c8777702
imvetri <svetrivel@gmail.com>
2021-01-24 19:06:45 +0530

    when user edits or selects a new type, onChange event is run with selected Type information

--- src/Type/Index.js
+++ src/Type/Index.js
@@ -10,13 +10,54 @@ class Type extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            types: []
         };
+    }
+
+    updateChanges(e){
+        var types = this.state.types;
+        types[Number(e.currentTarget.getAttribute("index"))] = e.currentTarget.value;
+        this.setState({
+            types: types
+        })
+        this.onChange();
+    }
+
+    addInput(){
+        var types = this.state.types;
+        types.push("")
+        this.setState(
+            {types:types}
+        )
+    }
+
+    deleteIfEmpty(e){
+        var types = this.state.types;
+        if(e.currentTarget.value===""){
+            types.splice(Number(e.currentTarget.getAttribute("index")),1)
+        }
+        this.setState({
+            types: types
+        })
+    }
+
+    onChange(){
+        if(this.props.onChange){
+            this.props.onChange(this.state)
+        }
     }
 
     render() {
         return (
-            <div>
-                Type
+            <div className="panel">
+                <header>Type</header>
+                <div className="list">
+                    {this.state.types.map((type, index)=><textarea value={type} key={index} index={index} 
+                        onChange={this.updateChanges.bind(this)} 
+                        onBlur={this.deleteIfEmpty.bind(this)}
+                        onFocus={this.onChange.bind(this)}/>)}
+                </div>
+                <button onClick={this.addInput.bind(this)}>Add</button>
             </div>
         );
     }
